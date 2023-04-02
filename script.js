da0271bcfd2b08bf27367a8f1408d65f98daadfe
Legendoflaohan <115490550+Legendoflaohan@users.noreply.github.com>
2023-04-02 23:14:19 +0800

    introduce sound effect, add subtraction feature

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 let text = [];
 
 //sum up two numbers
-function sum(a, b) {
+function addition(a, b) {
 
     return a + b;
 
+}
+
+function subtraction(a, b) {
+
+    return a - b;
+    
 }
 
 //add a num that been clicked in array(text) ;
@@ -15,14 +21,24 @@ function getText(e) {
     text.push(input);
 }
 
-function executeSum(indexOfAdd) {
-    //get two arguments for sum
-    let a = +text.slice(0, [indexOfAdd]).join('');
-    console.log(a)
-    let b = +text.slice([indexOfAdd], (text.length)).join('');
-    console.log(b)
-    //execute sum
-    let result = sum(a, b);
+function executeOperation(index) {
+    //get two arguments for operation
+    let a = +text.slice(0, [index]).join('');
+    let b = +text.slice([index + 1], (text.length)).join('');
+    let result = 0;
+    //select operation
+
+    console.log(text[index])
+    switch (true) {
+        case (text[index] == '+'):
+            result = addition(a, b);
+            break;
+        case (text[index] == '-'):
+            console.log(a);
+            console.log(b);
+            result = subtraction(a, b);
+            break;
+    }
 
     document.getElementById('mainScreen').innerText = `${result}`;
 }
@@ -31,16 +47,18 @@ function execute(e) {
     getText(e);
     //locate 'enter'
     let indexOfEnter = text.indexOf('enter');
-    //locate '+'
-    let indexOfAdd = text.indexOf('+');
-    console.log(indexOfAdd)
+
     //execute when 'enter' is clicked
     if (indexOfEnter != -1) {
         //remove 'enter'
         text.pop();
-        //execute if there is a '+'
-        if (indexOfAdd != -1) {
-            executeSum(indexOfAdd);
+        switch (true) {
+            case (text.indexOf('+') != -1):
+                executeOperation(text.indexOf('+'));
+                break;
+            case (text.indexOf('-') != -1):
+                executeOperation(text.indexOf('-'));
+                break;
         }
     }
 }
@@ -48,10 +66,27 @@ function execute(e) {
 function interFace(e) {
 
     if (e.target.innerText != 'enter') {
+
         document.getElementById('upperScreen').innerText += e.target.innerText;
     }
+}
+
+function soundEffect(e) {
+    switch (e.target.innerText) {
+        case '0':
+            document.getElementById('soundOne').pause();
+            document.getElementById('soundZero').currentTime = 0;
+            document.getElementById('soundZero').play();
+            break;
+        case '1':
+            document.getElementById('soundZero').pause();
+            document.getElementById('soundOne').currentTime = 0;
+            document.getElementById('soundOne').play();
+    }
+
 }
 
 //return text from element that been clicked
 addEventListener('click', interFace);
 addEventListener('click', execute);
+addEventListener('click', soundEffect);
